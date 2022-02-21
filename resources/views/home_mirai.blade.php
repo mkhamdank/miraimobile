7840246f1c45c2423672213b11bd0d860f3eda99
mkhamdank <gsyn153@gmail.com>
2022-02-21 07:47:19 +0700

    survey covid 27

--- resources/views/home_mirai.blade.php
+++ resources/views/home_mirai.blade.php
@@ -942,7 +942,7 @@ header("Pragma: no-cache");
 								<form class="contact100-form validate-form" style="padding: 0px 25px 58px 25px;display: none" id="form_belum_survey">
 									<div style="width: 100%;">
 										<div class="col-xs-12 col-md-12">
-											<center style="font-size:16px">Dear <span class="name_survey"></span><br>Survey Dapat Diisi Pada <span style="color: red">Minggu, 20 Februari 2022 Pukul 12:00 - 18:00 </span> <br></center>
+											<center style="font-size:16px">Dear <span class="name_survey"></span><br>Survey Dapat Diisi Pada <span style="color: red">Minggu, 27 Februari 2022 Pukul 12:00 - 18:00 </span> <br></center>
 										</div>
 									</div>
 
@@ -1379,7 +1379,7 @@ header("Pragma: no-cache");
 					}
 
 					if (index === 5) {
-						if (tgl >= "2022-02-20 11:00:00" && tgl <= "2022-02-20 18:00:00") {
+						if (tgl >= "2022-02-27 11:00:00" && tgl <= "2022-02-27 18:00:00") {
 							$('#form_kehadiran').hide();
 							$('#form_menu').hide();
 							$("#form_login").hide();
